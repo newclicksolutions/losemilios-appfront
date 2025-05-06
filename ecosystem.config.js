3f15115e325589a13d6c03afb33f83943b0da5ac
Luis Caicedo <lcaicedo@newclicksoluciones.com>
2025-05-05 20:09:05 -0500

    some fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
-// ecosystem.config.js
 module.exports = {
     apps: [
       {
         name: "losemilios-FRONT",
         script: "serve",
-        args: "-s dist -l 6004"
+        args: "-s dist -l 6004",
+        cwd: ".", // Ruta donde está la carpeta dist (generalmente en la raíz del proyecto Vue)
+        env: {
+          NODE_ENV: "production"
+        }
       }
     ]
-  }
+  };
